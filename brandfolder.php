ee9e6dcaff945e3ed370d65da57e1292f82eb2a5
parterburn <parterburn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-07 23:43:53 +0000

    changing out 0.1
    git-svn-id: https://plugins.svn.wordpress.org/brandfolder/trunk@664985 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ brandfolder.php
@@ -0,0 +1,176 @@
+<?php
+/*
+Plugin Name: brandfolder
+Plugin URI: http://brandfolder.com
+Description: Adds the ability for you to edit your brandfolder inside Wordpress as well as embed it as a popup or in a Page/Post.
+Version: 0.1
+Author: Brandfolder, Inc.
+Author URI: http://brandfolder.com
+License: GPLv2
+*/
+
+function brandfolder_shortcode()	{
+
+	$devOptions = get_option("brandfolderWordpressPluginAdminOptions");
+	if (!empty($devOptions)) {
+		foreach ($devOptions as $key => $option)
+			$brandfolderAdminOptions[$key] = $option;
+	}		
+
+	$brandfolder_url = $brandfolderAdminOptions["brandfolder_url"];
+	$output = '<iframe src="https://brandfolder.com/'.$brandfolder_url.'/embed" style="width: 100%; height: 100%; min-height: 600px;border:0px;" frameborder="0"></iframe>';
+	
+	return $output;
+
+ 
+}
+
+add_shortcode('brandfolder', 'brandfolder_shortcode');
+
+function add_brandfolder_button() {
+   // Don't bother doing this stuff if the current user lacks permissions
+   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+     return;
+ 
+   // Add only in Rich Editor mode
+   
+   if ( get_user_option('rich_editing') == 'true') {
+     add_filter("mce_external_plugins", "add_brandfolder_tinymce_plugin");
+     add_filter('mce_buttons', 'register_brandfolder_button');
+   }
+   
+}
+ 
+function register_brandfolder_button($buttons) {
+   array_push($buttons, "|", "yourbrandfolder");
+   return $buttons;
+}
+ 
+// Load the TinyMCE plugin : editor_plugin.js (wp2.5)
+function add_brandfolder_tinymce_plugin($plugin_array) {   
+   $plugin_array['yourbrandfolder'] = plugins_url( 'editor_plugin.js' , __FILE__ );
+   return $plugin_array;
+}
+
+
+function my_refresh_mce($ver) {
+  $ver += 3;
+  return $ver;
+}
+
+// init process for button control - NOT USING FOR BRANDFOLDER
+add_filter( 'tiny_mce_version', 'my_refresh_mce');
+add_action('init', 'add_brandfolder_button');
+
+
+if (!class_exists("brandfolderWordpressPlugin")) {
+	class brandfolderWordpressPlugin {
+		var $adminOptionsName = "brandfolderWordpressPluginAdminOptions";
+		function brandfolderWordpressPlugin() { //constructor
+			
+		}
+		function init() {
+			$this->getAdminOptions();
+		}
+		//Returns an array of admin options
+		function getAdminOptions() {
+			$devloungeAdminOptions = array('brandfolder_url' => '');
+			$devOptions = get_option($this->adminOptionsName);
+			if (!empty($devOptions)) {
+				foreach ($devOptions as $key => $option)
+					$devloungeAdminOptions[$key] = $option;
+			}				
+			update_option($this->adminOptionsName, $devloungeAdminOptions);
+			return $devloungeAdminOptions;
+		}
+		
+
+		//Prints out the admin page
+		function printAdminPage() {
+					$devOptions = $this->getAdminOptions();
+										
+					if (isset($_POST['update_brandfolderWordpressPluginSettings'])) { 
+						if (isset($_POST['brandfolder_url'])) {
+							$devOptions['brandfolder_url'] = apply_filters('brandfolder_url', $_POST['brandfolder_url']);
+						}
+						update_option($this->adminOptionsName, $devOptions);
+						
+						?>
+						<div class="updated"><p><strong><?php _e("Settings Updated.", "brandfolderWordpressPlugin");?></strong></p></div>
+											<?php
+											} ?>
+						<div class=wrap>
+						<form method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+						<h2>brandfolder setup</h2>
+						<h3>brandfolder url <span style="font-size:70%;">(get yours <a href="https://brandfolder.com/brands/" target="_blank">here</a>)</span></h3>
+						https://brandfolder.com/<input type="text" name="brandfolder_url" size="20" value="<?php _e(apply_filters('format_to_edit',$devOptions['brandfolder_url']), 'brandfolderWordpressPlugin') ?>">
+						<div class="submit">
+						<input type="submit" name="update_brandfolderWordpressPluginSettings" value="<?php _e('Update Settings', 'brandfolderWordpressPlugin') ?>" /></div>
+						</form>
+						<h3>Security Settings</h3>
+						<p>Restrict embedding to certain domains by clicking 'Embed' for your brand at <a href="https://brandfolder.com/brands" target="_blank">https://brandfolder.com/brands</a> and fill out the "Embed Restrictions" box.</p>
+						 </div>
+					<?php
+				}//End function printAdminPage()
+
+
+		function Main() {
+			    if (!current_user_can('manage_options'))  {
+			        wp_die( __('You do not have sufficient permissions to access this page.') );
+			    }
+			  	echo '<iframe src="https://brandfolder.com" style="width: 98%; height: 90%; min-height: 600px;margin-top:10px;"></iframe>';
+			}
+
+		function ConfigureMenu() {
+			add_menu_page("brandfolder", "brandfolder", 6, basename(__FILE__), array(&$dl_pluginSeries,'Main'));
+			add_submenu_page( "brandfolder-menu", "plugin setup", "plugin setup", 6, basename(__FILE__),  array(&$dl_pluginSeries,'printAdminPage') );
+		}			
+
+		function add_settings_link($links, $file) {
+		static $this_plugin;
+		if (!$this_plugin) $this_plugin = plugin_basename(__FILE__);
+		 
+		if ($file == $this_plugin){
+			$settings_link = '<a href="admin.php?page=brandfolder-sub-menu">'.__("setup", "brandfolder-wordpress-plugin").'</a>';
+			 array_unshift($links, $settings_link);
+		}
+			return $links;
+		 }
+	
+	}
+
+} //End Class brandfolderWordpressPlugin
+
+if (class_exists("brandfolderWordpressPlugin")) {
+	$dl_pluginSeries = new brandfolderWordpressPlugin();
+}
+
+//Initialize the admin panel
+if (!function_exists("brandfolderWordpressPlugin_ap")) {
+	function brandfolderWordpressPlugin_ap() {
+		global $dl_pluginSeries;
+		if (!isset($dl_pluginSeries)) {
+			return;
+		}
+
+		add_menu_page("brandfolder", "brandfolder", 6, "brandfolder-menu", array(&$dl_pluginSeries,'Main'), plugin_dir_url(__FILE__)."favicon.png");
+		add_submenu_page( "brandfolder-menu", "plugin setup", "plugin setup", 6, "brandfolder-sub-menu",  array(&$dl_pluginSeries,'printAdminPage') );
+
+	}	
+}
+
+//Actions and Filters	
+if (isset($dl_pluginSeries)) {
+	//Actions
+	add_action('admin_menu', 'brandfolderWordpressPlugin_ap');
+	add_action('brandfolder-wordpress-plugin/brandfolder.php',  array(&$dl_pluginSeries, 'init'));
+	
+//	add_filter('plugin_action_links', array(&$dl_pluginSeries, 'add_settings_link'), 10, 2 );
+
+    wp_register_script( 'brandfolder', 'https://brandfolder.com/js');
+    wp_enqueue_script( 'brandfolder'); 
+
+}
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== brandfolder - own your brand ===
+Contributors: parterburn
+Tags: brand assets management, brand management, share your brand, logo upload, logo management
+Requires at least: 3.0
+Tested up to: 3.5.1
+Stable tag: 0.1
+
+Our simple plugin creates a beautiful web view for brand consumers to access your logos, product images, brand information, and plenty more.
+
+== Description ==
+
+> This plugin requires a brandfolder account which you can setup at <a href="http://www.brandfolder.com" rel="nofollow">brandfolder.com</a>
+
+This plugin uses brandfolder to make it as easy as possible to create a press kit.
+
+### Features of the brandfolder WordPress plugin
+
+* Edit your brandfolder directly from your WordPress admin panel
+* Designate links to your brandfolder with the class name 'brandfolder' and you'll quickly have the embed popup working
+* Embed your interactive brandfolder inside a Page or Post using the [brandfolder] shortcode (make sure you have added your unique brandfoler URL befor doing so)
+
+
+== Installation ==
+
+1. Unpack archive to this archive to the 'wp-content/plugins/' directory inside of WordPress
+2. From your blog administration, click on brandfolder to change settings (will likely show up as a main tab below 'Settings')
+
+== Support ==
+
+* Visit https://brandfolder.uservoice.com for support & documentation.
+
+* We also recommend the [WordPress HelpCenter](http://wphelpcenter.com/) for extended help. Brandfolder is not associated with the WordPress HelpCenter in any way.
+
+= What is the plugin license? =
+This plugin is released under a GPL license.
+
+== Changelog ==
+
+= 0.1 =
+* We're just getting started here!
