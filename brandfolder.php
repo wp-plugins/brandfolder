24a776fc9686000f778cf353d56ebfa1f9dc129f
parterburn <parterburn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-14 03:29:34 +0000

    Version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/brandfolder/trunk@755899 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- brandfolder.php
+++ brandfolder.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Brandfolder
-Plugin URI: http://brandfolder.com
+Plugin URI: http://wordpress.org/plugins/brandfolder/
 Description: Adds the ability for you to edit your brandfolder inside Wordpress as well as embed it as a popup or in a Page/Post.
-Version: 1.0.3
+Version: 1.1
 Author: Brandfolder, Inc.
 Author URI: http://brandfolder.com
 License: GPLv2
@@ -215,7 +215,7 @@ class BrandfolderServe {
 
 	  <?php
 
-	      $output = '<iframe id="bfIframe" name="bfIframe" src="https://brandfolder.com/search" style="background-color:white;background-image:url(\'https://d2sdf28wg0skh3.cloudfront.net/loading_embed.gif\');background-repeat:no-repeat;background-attachment:fixed;background-position:center;width: 99%; height:85%; min-height: 600px;border:0px;border:2px solid #CCC;margin:0 auto;" frameborder="0"></iframe>';  
+	      $output = '<iframe id="bfIframe" name="bfIframe" src="https://brandfolder.com/search" style="background-color:white;background-image:url(\'https://d2sdf28wg0skh3.cloudfront.net/loading_embed.gif\');background-repeat:no-repeat;background-attachment:fixed;background-position:center;width: 99%; height:85%; min-height: 750px;border:0px;border:2px solid #CCC;margin:0 auto;" frameborder="0"></iframe>';  
 
 	      echo $output;
 	      echo "</div>";
@@ -295,8 +295,23 @@ function brandfolder_shortcode()	{
 	}
 
 	$brandfolder_url = $brandfolderAdminOptions["brandfolder_url"];
-	$powered_by = $brandfolderAdminOptions["powered_by"];
-	$output = '<iframe src="https://brandfolder.com/'.$brandfolder_url.'/embed" style="width: 100%; height: 100%; min-height: 600px;border:0px;border:2px solid #CCC;" frameborder="0"></iframe>';
+	$brandfolder_powered_by = $brandfolderAdminOptions["brandfolder_powered_by"];
+	$brandfolder_inline_height = $brandfolderAdminOptions["brandfolder_inline_height"];
+	$brandfolder_inline_width = $brandfolderAdminOptions["brandfolder_inline_width"];
+
+	if (isset($brandfolder_inline_height)) {
+		$bf_inline_height = $brandfolder_inline_height;
+	} else {
+		$bf_inline_height = "750px;";
+	}
+
+	if (isset($brandfolder_inline_width)) {
+		$bf_inline_width = $brandfolder_inline_width;
+	} else {
+		$bf_inline_width = "99%;";
+	}	
+
+	$output = '<iframe src="https://brandfolder.com/'.$brandfolder_url.'/embed" style="max-width: '.$bf_inline_width.' !important;width: '.$bf_inline_width.' !important; height: '.$bf_inline_height.' !important; min-height: '.$bf_inline_height.' !important;border:0px;border:2px solid #CCC;" frameborder="0"></iframe>';
 	$output .= '<a href="http://brandfolder.com" title="Brand assets by Brandfolder" style="float:right;"><img src="//d2sdf28wg0skh3.cloudfront.net/powered_by.png" style="height:30px;border:0px;"></a>';
 	$output .= '<div style="clear:both;"></div>';	
 
@@ -369,7 +384,9 @@ if (!class_exists("brandfolderWordpressPlugin")) {
 					if (isset($_POST['update_brandfolderWordpressPluginSettings'])) { 
 						if (isset($_POST['brandfolder_url'])) {
 							$devOptions['brandfolder_url'] = apply_filters('brandfolder_url', $_POST['brandfolder_url']);
-							$devOptions['powered_by'] = apply_filters('powered_by', $_POST['powered_by']);
+							$devOptions['brandfolder_powered_by'] = apply_filters('brandfolder_powered_by', $_POST['brandfolder_powered_by']);
+							$devOptions['brandfolder_inline_height'] = apply_filters('brandfolder_inline_height', $_POST['brandfolder_inline_height']);
+							$devOptions['brandfolder_inline_width'] = apply_filters('brandfolder_inline_width', $_POST['brandfolder_inline_width']);
 						}
 						update_option($this->adminOptionsName, $devOptions);
 						
@@ -382,6 +399,30 @@ if (!class_exists("brandfolderWordpressPlugin")) {
 						<h2>Brandfolder setup</h2>
 						<h3>Brandfolder url <span style="font-size:70%;">(get yours <a href="https://brandfolder.com/brands/" target="_blank">here</a>)</span></h3>
 						https://brandfolder.com/<input type="text" name="brandfolder_url" size="20" value="<?php _e(apply_filters('format_to_edit',$devOptions['brandfolder_url']), 'brandfolderWordpressPlugin') ?>">
+						<br>
+						<hr>
+						<h3>Settings for inline-embed option <span style="font-size:70%;">(<a href="http://help.brandfolder.com/knowledgebase/articles/237579" target="_blank">what's this?</a>)</span></h3>
+						<div class="">
+							<?php
+								if(isset($devOptions['brandfolder_inline_height'])) {
+									$brandfolder_inline_height = $devOptions['brandfolder_inline_height'];
+								} else {
+									$brandfolder_inline_height = "750px";
+								}
+								if(isset($devOptions['brandfolder_inline_width'])) {
+									$brandfolder_inline_width = $devOptions['brandfolder_inline_width'];
+								} else {
+									$brandfolder_inline_width = "99%";
+								}								
+							?>
+							IFrame Height: <input type="text" name="brandfolder_inline_height" size="20" value="<?php echo $brandfolder_inline_height ?>"><span style="font-size:90%;margin-left:15px;">Ex) 750px or 100%</span>
+							<br>
+							IFrame Width: <input type="text" name="brandfolder_inline_width" size="20" value="<?php echo $brandfolder_inline_width ?>"><span style="font-size:90%;margin-left:15px;">Ex) 750px or 100%</span>
+						</div>
+						<br>
+						<hr>
+						<h3 style="color:#CCC;"><input name="brandfolder_powered_by" disabled="disabled" type="checkbox" value="1" <?php checked( '1', $devOptions['brandfolder_powered_by'] ); ?>> Remove "Powered by Brandfolder" link <span style="font-size:70%;">(future paid feature)</span></h3>
+						
 						<div class="submit">
 						<input type="submit" name="update_brandfolderWordpressPluginSettings" value="<?php _e('Update Settings', 'brandfolderWordpressPlugin') ?>" /></div>
 						</form>
@@ -404,9 +445,9 @@ if (!class_exists("brandfolderWordpressPlugin")) {
 					$brandfolder_url = $brandfolderAdminOptions["brandfolder_url"];
 
 			    if ($brandfolder_url == "") {
-						echo '<iframe src="https://brandfolder.com" style="width: 98%; height: 95%; min-height: 600px;margin-top:10px;"></iframe>';
+						echo '<iframe src="https://brandfolder.com" style="width: 98%; height: 95%; min-height: 730px;margin-top:10px;"></iframe>';
 			    } else {
-			    	echo '<iframe src="https://brandfolder.com/'.$brandfolder_url.'/edit" style="width: 98%; height: 95%; min-height: 600px;margin-top:10px;"></iframe>';	
+			    	echo '<iframe src="https://brandfolder.com/'.$brandfolder_url.'/edit" style="width: 98%; height: 95%; min-height: 730px;margin-top:10px;"></iframe>';	
 			    } 	
 			}
 
